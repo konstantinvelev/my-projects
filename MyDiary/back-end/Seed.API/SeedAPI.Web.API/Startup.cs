08797fa2c288580a6da93fec8aa901fb162a1ae4
Konstantin <koceto.velev12@gmail.com>
2021-05-04 15:41:25 +0200

    Add and write TokenController.cs and add UserViewModel.cs

--- MyDiary/back-end/Seed.API/SeedAPI.Web.API/Startup.cs
+++ MyDiary/back-end/Seed.API/SeedAPI.Web.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SeedAPI.Web.API.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,12 @@ namespace SeedAPI.Web.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //TODO
+
+            //DependencyInjectionConfig.AddScope(services);
+            //JwtTokenConfig.AddAuthentication(services, Configuration);
+            //DBContextConfig.Initialize(services, Configuration);
+            //services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

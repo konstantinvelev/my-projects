d5bc23eaf3fb4730d874394ae59777d37722ccd6
Konstantin Velev <ABZQNZZ@mmm.com>
2021-12-14 16:01:23 +0200

    Change name and theme

--- CarDealer/server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const authController = require('./auth');
-const postController = require('./postController');
-const commentController = require('./commentController');
-
-module.exports = {
-    authController,
-    postController,
-    commentController,
-}
